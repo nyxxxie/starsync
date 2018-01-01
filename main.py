21b9cd4f462f6c9811c2b6ecc7a92272a74b7ae8
Nyxxie <nyxxxxie@gmail.com>
2018-01-01 17:01:50 -0500

    Fixed mixed string quote chars.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ os.makedirs(OUT_DIR, exist_ok=True)
 print('Fetching repo list.')
 repos = []
 for i in range(1, 100000):  # hopefully I never star more than 100000 pages of  repos...
-    r = requests.get("https://api.github.com/users/nyxxxie/starred?page={}".format(i))
+    r = requests.get('https://api.github.com/users/nyxxxie/starred?page={}'.format(i))
     j = json.loads(r.text)
     if len(j) == 0:
         break
@@ -18,7 +18,7 @@ for i in range(1, 100000):  # hopefully I never star more than 100000 pages of  
     for repo in j:
         if repo['private']:
             continue
-        repos.append(repo["html_url"])
+        repos.append(repo['html_url'])
 
 print('Cloning {} repositories.'.format(len(repos)))
 for repo in tqdm.tqdm(repos):
